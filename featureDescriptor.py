6da0d76c7189faf1ce86c97c0d7d461d25e6d961
Derek Nien <aegis60918@gmail.com>
2015-04-29 17:47:58 +0800

    - Add: featureDescriptor

---
+++ featureDescriptor.py
@@ -0,0 +1,11 @@
+PATCH_SIZE = 9
+
+def getFeaturePatch(grayImage, features):
+    imageHeight, imageWidth = grayImage.shape
+    featurePatches = []
+    for feature in features:
+        if (feature[0] - PATCH_SIZE >= 0) and (feature[0] + PATCH_SIZE < imageHeight) and (feature[1] - PATCH_SIZE >= 0) and (feature[1] + PATCH_SIZE < imageWidth):
+            featurePatch = grayImage[feature[0] - PATCH_SIZE / 2: feature[0] + PATCH_SIZE / 2 + 1, feature[1] - PATCH_SIZE / 2: feature[1] + PATCH_SIZE / 2 + 1]
+            featurePatch = featurePatch.reshape(PATCH_SIZE * PATCH_SIZE)
+            featurePatches.append(featurePatch)
+    return featurePatches
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import featureDetect as fd
+import featureDescriptor as fdes
 
 ORIGINAL_IMAGE_PATH = 'original_images/'
 
@@ -17,6 +18,7 @@ for i in range(TOTAL_IMAGES):
     colorImage = cv2.transpose(colorImage)
     grayImage = cv2.cvtColor(np.float32(colorImage), cv2.COLOR_BGR2GRAY)
     features = fd.detectFeatures(grayImage)
+    featurePatch = fdes.getFeaturePatch(grayImage, features)
     imageFeatures.append(features)
     for feature in features:
         cv2.circle(colorImage, feature[::-1], 10, (255, 0, 0), -1)
